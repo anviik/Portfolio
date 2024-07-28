4de2b07397ee72b97e050b5ff62ef04db855f8cc
Anvi <anvikudaraya417@gmail.com>
2024-07-27 23:19:29 -0700

    change background

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var timeline = document.querySelector('.timeline');
-    var timelinePosition = timeline.offsetTop - window.innerHeight + 100; // Adjust this value if needed
-    var animationTriggered = false;
+let navLinks = document.querySelectorAll('header nav a');
 
-    function checkTimelineVisibility() {
-        if (window.scrollY > timelinePosition && !animationTriggered) {
-            timeline.classList.add('animate-timeline');
-            animationTriggered = true;
-            window.removeEventListener('scroll', checkTimelineVisibility);
+window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+
+    document.querySelectorAll('section').forEach(sec => {
+        let top = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (scrollPosition >= top && scrollPosition < top + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            console.log("id", id)
+            document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+        } else {
+            document.querySelector('header nav a[href="#' + id + '"]').classList.remove('active');
         }
-    }
-
-    window.addEventListener('scroll', checkTimelineVisibility);
-    checkTimelineVisibility(); // Check visibility on page load
+    });
 });
 
 
-
-// window.onscroll = () => {
-// let header = document.querySelector('.header');
-// header.classList.toggle('sticky', window.scrollY > 100);
-// };
-
-// let darkModeIcon = document.querySelector('#darkMode-icon');
-
-// darkModeIcon.oneclick = () => { 
-//     darkModeIcon.classList.toggle('bx-sun');
-//     document.body.classList.toggle('dark-mode');
-// } 
